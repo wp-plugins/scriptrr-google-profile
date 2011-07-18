b13052dfbe9607023530be097cd8f29eb92baea5
svnlabs <svnlabs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-18 03:39:01 +0000

    git-svn-id: https://plugins.svn.wordpress.org/scriptrr-google-profile/trunk@411254 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,53 @@
+=== Plugin Name ===
+
+Scriptrr Google + Profile widget Wordpress Plugin: http://plus.scriptrr.com/
+
+Plugin Name: Scriptrr Google + Profile widget
+Plugin URI: http://plus.scriptrr.com/
+Description: Google Plus Profile Widget allows users to add plugin on their blog or website to invite visitors to new Google + Profile. 
+Author: Sandeep Verma
+Contributors: Sandeep Verma
+Donate link: http://plus.scriptrr.com/ 
+Version: 0.7
+Tags: scriptrr google plus profile widget
+Requires at least: 2.0.2
+Tested up to: 2.1
+Stable tag: 4.3
+Author URI: http://blog.svnlabs.com
+Other: Plus.scriptrr.com is a tool to generate plugin/widget for Google + Profile. Plus.scriptrr.com free, it does not require your personal information. 100+ users already using this. Plus.scriptrr.com is not a Google product.
+
+== Description ==
+
+Google Plus Profile Widget allows users to add plugin on their blog or website to invite visitors to new Google + Profile.
+
+Please send us an email to scriptrr@gmail.com for your questions or suggestions. 
+
+Plus.scriptrr.com is a tool to generate plugin/widget for Google + Profile. Plus.scriptrr.com free, it does not require your personal information. 100+ users already using this. Plus.scriptrr.com is not a Google product.
+
+== Installation ==
+
+1. Download scriptrr_google_plus_profile_widget.zip
+2. Extract and upload scriptrr_google_plus_profile_widget.php to the plugins/ directory
+3. Enable scriptrr_google_plus_profile_widget Widget in the Plugin admin panel
+4. Place scriptrr_google_plus_profile_widget in the sidebar, and edit it to enter the Google + Profile plugin.
+
+
+== Screenshots ==
+
+1. Screenshot of Google + Profile Widget in Front End.
+
+2. Screenshot of admin panel widget area.
+
+
+== Arbitrary section ==
+
+Download Plugin: http://plus.scriptrr.com/
+
+Follow me:
+
+Facebook: http://www.facebook.com/svnlabs
+Twitter: http://www.twitter.com/svnlabs
+
+Subscribe me:
+Youtube: http://www.youtube.com/user/svnlabs
+Feeds: http://blog.svnlabs.com/feed/
---
+++ scriptrr_google_plus_profile_widget.php
@@ -0,0 +1,119 @@
+<?php
+/*
+Plugin Name: Scriptrr Google + Profile widget
+Plugin URI: http://plus.scriptrr.com/
+Description: Google Plus Profile Widget allows users to add plugin on their blog or website to invite visitors to new Google + Profile. 
+Author: Sandeep Verma
+Version: 0.7
+Author URI: http://blog.svnlabs.com
+Other: Plus.scriptrr.com is a tool to generate plugin/widget for Google + Profile. Plus.scriptrr.com free, it does not require your personal information. 100+ users already using this. Plus.scriptrr.com is not a Google product.
+*/
+
+
+// Put functions into one big function we'll call at the plugins_loaded
+// action. This ensures that all required plugin functions are defined.
+function widget_scriptrr_google_plus_profile_widget_init() {
+
+	// Check for the required plugin functions. This will prevent fatal
+	// errors occurring when you deactivate the dynamic-sidebar plugin.
+	if ( !function_exists('register_sidebar_widget') )
+		return;
+
+	// This is the function that outputs our little Google search form.
+	function widget_scriptrr_google_plus_profile_widget($args) {
+		
+		// $args is an array of strings that help widgets to conform to
+		// the active theme: before_widget, before_title, after_widget,
+		// and after_title are the array keys. Default tags: li and h2.
+		extract($args);
+		
+		
+		// Each widget can store its own options. We keep strings here.
+		$options = get_option('widget_scriptrr_google_plus_profile_widget');
+		$userid = $options['userid'];
+		$width = $options['width'];
+		$host = $options['host'];
+		$title = ($options['title'] != "") ? $before_title.$options['title'].$after_title : "";  
+		$widget = html_entity_decode($options['widget']);
+		
+		// These lines generate our output. Widgets can be very complex
+		// but as you can see here, they can also be very, very simple.
+		//echo $before_widget . $before_title . $title . $after_title;
+		
+
+			 
+		echo $before_widget;
+		echo $title;
+		echo $widget;
+		echo $after_widget;
+	
+	}
+
+	// This is the function that outputs the form to let the users edit
+	// the widget's title. It's an optional feature that users cry for.
+	function widget_scriptrr_google_plus_profile_widget_control() {
+
+		// Get our options and see if we're handling a form submission.
+		$options = get_option('widget_scriptrr_google_plus_profile_widget');
+		
+		
+		if ( !is_array($options) )
+			$options = array('title'=>'Google Plus Profile Widget', 
+				
+				'userid' => '100825987461531739433',
+				'width' => '200',
+				'host' => 'plus.scriptrr.com',
+				
+			);
+			
+			
+		if ( $_POST['scriptrr_google_plus_profile_widget-submit'] ) {
+
+			// Remember to sanitize and format use input appropriately.
+			
+			$options['userid'] = strip_tags(stripslashes($_POST['scriptrr_google_plus_profile_widget-userid']));
+			$options['width'] = strip_tags(stripslashes($_POST['scriptrr_google_plus_profile_widget-width']));
+			$options['host'] = strip_tags(stripslashes($_POST['scriptrr_google_plus_profile_widget-host']));
+			
+						
+
+			$options['title'] = 'Follow me on Google+';
+			$options['widget'] = '<iframe src="http://plus.scriptrr.com/scriptrr.php?id='.$options['userid'].'&host='.$options['host'].'&width='.$options['width'].'" frameborder="0" scrolling="no" height="350" width="'.$options['width'].'"></iframe>';			
+			
+			
+			//  echo $result;
+           
+			
+			update_option('widget_scriptrr_google_plus_profile_widget', $options);
+		}
+
+		// Be sure you format your options to be valid HTML attributes.
+		
+		$userid = htmlspecialchars($options['userid'], ENT_QUOTES);
+		$width = htmlspecialchars($options['width'], ENT_QUOTES);
+		$host = htmlspecialchars($options['host'], ENT_QUOTES);
+
+		
+		// Here is our little form segment. Notice that we don't need a
+		// complete form. This will be embedded into the existing form.
+
+		echo '<p style="text-align:right;"><label for="scriptrr_google_plus_profile_widget-userid">' . __('Google+ ID:', 'widgets') . ' <input style="width: 200px;" id="scriptrr_google_plus_profile_widget-userid" name="scriptrr_google_plus_profile_widget-userid" type="text" value="'.$userid.'" /></label></p>';
+		echo '<p style="text-align:right;"><label for="scriptrr_google_plus_profile_widget-width">' . __('Width:', 'widgets') . ' <input style="width: 200px;" id="scriptrr_google_plus_profile_widget-width" name="scriptrr_google_plus_profile_widget-width" type="text" value="'.$width.'" /></label></p>';
+		echo '<p style="text-align:right;"><label for="scriptrr_google_plus_profile_widget-host">' . __('Domain:', 'widgets') . ' <input style="width: 200px;" id="scriptrr_google_plus_profile_widget-host" name="scriptrr_google_plus_profile_widget-host" type="text" value="'.$host.'" /></label></p>';
+
+		echo '<input type="hidden" id="scriptrr_google_plus_profile_widget-submit" name="scriptrr_google_plus_profile_widget-submit" value="1" />';
+	}
+	
+	// This registers our widget so it appears with the other available
+	// widgets and can be dragged and dropped into any active sidebars.
+	register_sidebar_widget(array('Scriptrr Google + Profile', 'widgets'), 'widget_scriptrr_google_plus_profile_widget');
+
+	// This registers our optional widget control form. Because of this
+	// our widget will have a button that reveals a 300x100 pixel form.
+	register_widget_control(array('Scriptrr Google + Profile', 'widgets'), 'widget_scriptrr_google_plus_profile_widget_control', 300, 200);
+}
+
+// Run our code later in case this loads prior to any required plugins.
+add_action('widgets_init', 'widget_scriptrr_google_plus_profile_widget_init');
+
+?>
